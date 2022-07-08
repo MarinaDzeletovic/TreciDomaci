8152e51e2cb6247a807c7bc9aad02f2956fa9982
MarinaDzeletovic <dzeletovic00@gmail.com>
2022-07-08 15:51:57 +0200

    Dodata strana sa konvertorom

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import valute from './valute.json';
+import Navbar from './components/Navbar'
+import { Route, Switch } from 'react-router';
+import Konvertor from './components/Konvertor';
+import SpisakValuta from './components/SpisakValuta';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Switch>
+        <Route path='/'>
+          <Konvertor valute={valute.data} />
+        </Route>
+      </Switch>
+    </>
+  );
+}
+
+export default App;
